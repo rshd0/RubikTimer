198482ebee76d1337620a174986e6a31e5f4fe8a
rashad <rashadmk50@gmail.com>
2026-06-24 17:05:20 +0330

    fix: allow toggling between DNF and +2

--- scripts/main.js
+++ scripts/main.js
@@ -100,10 +100,9 @@ dnf_button.addEventListener("click", function(){
     let current_time = timeEl.textContent;
     if (current_time !== "00.000"){
         let solves = JSON.parse(localStorage.getItem(settings.cube_order)) || [];
-        if (solves.length > 0 && solves[solves.length - 1].status !== plus_2){
+        if (solves.length > 0){
             solves[solves.length-1].status = "dnf"; 
             localStorage.setItem(settings.cube_order, JSON.stringify(solves));
-            
             updateStates(settings);
         }
         
@@ -116,11 +115,9 @@ plus_2.addEventListener("click", function(){
     if (current_time !== "00.000"){
         let solves = JSON.parse(localStorage.getItem(settings.cube_order)) || [];
         if (solves.length > 0){
-            if (solves[solves.length-1].status !== "dnf"){
-                solves[solves.length-1].status = "plus2";
-                localStorage.setItem(settings.cube_order, JSON.stringify(solves));
-                updateStates(settings);
-            }
+            solves[solves.length-1].status = "plus2";
+            localStorage.setItem(settings.cube_order, JSON.stringify(solves));
+            updateStates(settings);
         }
     }  
 })
